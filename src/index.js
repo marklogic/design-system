34195ca8e03bfb5793613b86a0e09bce69d2d6e7
Phoenix Eliot <phoenixjeliot@gmail.com>
2020-03-13 12:08:55 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import MLButton from './ml-button'
 import MLDatePicker from './ml-date-picker'
 import MLFooter from './ml-footer'
 import MLLayout from './ml-layout'
+import MLRadio from './ml-radio'
 import MLSlider from './ml-slider'
 
 export {
@@ -9,5 +10,6 @@ export {
   MLDatePicker,
   MLFooter,
   MLLayout,
+  MLRadio,
   MLSlider,
 }
