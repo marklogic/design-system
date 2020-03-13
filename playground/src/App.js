932533cc2aa91e6b3c9f9d69d29fecdb76d0ce12
Phoenix Eliot <phoenixjeliot@gmail.com>
2020-03-13 09:49:05 -0600

    Add examples to Playground/App.js

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
 
-import { MLButton, MLFooter } from 'marklogic-ui-library'
+import {
+  MLButton,
+  MLDatePicker,
+  MLLayout,
+  MLSlider,
+} from 'marklogic-ui-library'
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <MLButton type='primary'>Test</MLButton>
-        <MLFooter year='2019' />
+        <MLLayout>
+          <MLLayout.MLHeader>Header</MLLayout.MLHeader>
+          <MLLayout.MLContent>
+            <MLButton type='primary'>Test</MLButton>
+          </MLLayout.MLContent>
+          <MLLayout.MLFooter year='2019' />
+        </MLLayout>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import MLButton from './ml-button'
-import MLLayout from './ml-layout'
+import MLDatePicker from './ml-date-picker'
 import MLFooter from './ml-footer'
+import MLLayout from './ml-layout'
+import MLSlider from './ml-slider'
 
 export {
   MLButton,
+  MLDatePicker,
+  MLFooter,
   MLLayout,
-  MLFooter
+  MLSlider,
 }
