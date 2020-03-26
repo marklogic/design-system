0dd7a66d1a81ec4d6dc4df7a169f30d23fe812ea
Phoenix Eliot <phoenixjeliot@gmail.com>
2020-03-26 14:20:19 -0600

    Update playground

--- playground/config-overrides.js
+++ playground/config-overrides.js
@@ -1,17 +1,27 @@
-const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+const path = require('path')
+const { override, fixBabelImports, babelInclude, addLessLoader, removeModuleScopePlugin, babelExclude } = require('customize-cra')
 
 module.exports = override(
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
-     style: true,
+    style: true,
   }),
- addLessLoader({
-   javascriptEnabled: true,
-   modifyVars: { 
-     '@primary-color': '#127da1'
-    },
- }),
-);
+  addLessLoader({
+    javascriptEnabled: true,
+    // modifyVars: {
+    //   '@primary-color': '#127da1',
+    // },
+  }),
+  removeModuleScopePlugin(),
+  babelInclude([
+    path.resolve('src'),
+    path.resolve('node_modules/marklogic-ui-library'),
+    // path.resolve('../src'),
+  ]),
+  babelExclude([
+    path.resolve('node_modules/marklogic-ui-library/node_modules'),
+  ]),
+)
 // Refer to theme vars below
 // https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
--- playground/src/App.js
+++ playground/src/App.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react'
 
 import {
   MLButton,
-  MLDatePicker,
+  // MLDatePicker,
   MLLayout,
-  MLSlider,
-} from 'marklogic-ui-library'
+  // MLSlider,
+  MLRadio,
+  MLIcon,
+} from 'marklogic-ui-library/dist'
+
+import 'marklogic-ui-library/dist/styles.min.css'
 
 export default class App extends Component {
   render () {
@@ -15,6 +19,21 @@ export default class App extends Component {
           <MLLayout.MLHeader>Header</MLLayout.MLHeader>
           <MLLayout.MLContent>
             <MLButton type='primary'>Test</MLButton>
+            <MLRadio.MLGroup name='radiogroup'>
+              <MLRadio name='radiogroup2' value='A2'>A2</MLRadio>
+            </MLRadio.MLGroup>
+            <MLRadio.MLGroup name='radiogroup'>
+              <MLRadio name='radiogroup2' value='B2'>B2</MLRadio>
+            </MLRadio.MLGroup>
+            <MLRadio.MLGroup name='radiogroup'>
+              <MLRadio name='radiogroup2' value='C2'>C2</MLRadio>
+            </MLRadio.MLGroup>
+            <MLRadio.MLGroup name='radiogroup2'>
+              <MLRadio name='radiogroup' value='A'>A</MLRadio>
+              <MLRadio name='radiogroup' value='B'>B</MLRadio>
+              <MLRadio name='radiogroup' value='C'>C</MLRadio>
+            </MLRadio.MLGroup>
+            <MLIcon.Abacus />
           </MLLayout.MLContent>
           <MLLayout.MLFooter year='2019' />
         </MLLayout>
