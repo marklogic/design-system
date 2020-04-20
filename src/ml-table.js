fb1d74f27d5e33e768636713c9ee12f39bcf426a
Phoenix Eliot <phoenixjeliot@gmail.com>
2020-04-20 15:21:20 -0600

    Use lodash-es for package build

--- src/ml-table.js
+++ src/ml-table.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Descriptions, Table } from 'antd'
-import { cloneDeep, isUndefined, isArray, merge } from 'lodash'
+import { cloneDeep, isUndefined, isArray, merge } from 'lodash-es'
 import './ml-table.css'
 import { DownOutlined, RightOutlined } from './ml-icon'
 
