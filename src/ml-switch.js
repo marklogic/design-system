c8cd5fc8f46d850fb8e0d6cc70575fa2fb42e921
Dan Lipsitt <danlipsitt@gmail.com>
2020-04-22 14:29:26 -0700

    Add props and knobs.

--- src/ml-switch.js
+++ src/ml-switch.js
@@ -1,10 +1,21 @@
 import { Switch } from 'antd'
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const MLSwitch = (params) => {
   return <Switch {...params} />
 }
 
-MLSwitch.propTypes = Switch.propTypes
+export const sizes = ['small', 'default']
+
+MLSwitch.propTypes = {
+  checked: PropTypes.bool,
+  size: PropTypes.oneOf(sizes),
+}
+
+MLSwitch.defaultProps = {
+  checked: false,
+  size: 'small',
+}
 
 export default MLSwitch
--- stories/41-Switch.stories.js
+++ stories/41-Switch.stories.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { boolean, radios, withKnobs } from '@storybook/addon-knobs'
 import { MLSwitch } from 'marklogic-ui-library'
+import { sizes } from 'marklogic-ui-library/ml-switch'
 
 export default {
   component: MLSwitch,
-  title: 'Switch',
+  title: 'Data Entry/MLSwitch',
+  decorators: [withKnobs],
 }
 
 export const Basic = () => {
-  return <MLSwitch />
+  return <MLSwitch
+    checked={boolean('Checked', true)}
+    size={radios('Size', sizes, 'small')}
+  />
 }
