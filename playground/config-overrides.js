cd450e9508f84678ca15660fd062e71d72b9fd2f
Phoenix Eliot <phoenixjeliot@gmail.com>
2020-05-01 22:26:18 -0600

    Clean up package.json and config-overrides

--- playground/config-overrides.js
+++ playground/config-overrides.js
@@ -15,10 +15,8 @@ module.exports = override(
     // https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
     modifyVars: themeVariables,
   }),
-  // This must include any source files that import @marklogic/design-system (or JSX, etc)
-  // that needs compilation
   babelInclude([
     path.resolve(__dirname, 'src'),
-    /marklogic\/design-system/, // Required for the library to compile
+    /marklogic\/design-system/, // Required for @marklogic/design-system to compile
   ]),
 )
