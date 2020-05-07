c1fd542fd1ba59c4df29e7a0b964052bd5cb6794
Dan Lipsitt <danlipsitt@gmail.com>
2020-05-07 10:51:51 -0700

    Demo MLModal alerts.

--- stories/25-Modal.stories.js
+++ stories/25-Modal.stories.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { MLModal, MLButton } from '@marklogic/design-system'
 import { withKnobs, text, boolean } from '@storybook/addon-knobs'
+import { Modal } from 'antd'
 
 export default {
   title: 'Feedback/MLModal',
@@ -16,7 +17,7 @@ export const Basic = () => {
         Open Modal
       </MLButton>
       <MLModal
-        title={text('title','Basic Modal')}
+        title={text('title', 'Basic Modal')}
         visible={visible}
         closable={boolean('closable', true)}
         onOk={() => setVisible(false)}
@@ -38,4 +39,48 @@ Basic.story = {
       storyDescription: 'Default MLModal has "Cancel" and "OK" buttons.',
     },
   },
+}
+
+function info() {
+  MLModal.info({
+    title: 'This is a notification message',
+    content: (
+      <div>
+        <p>some messages...some messages...</p>
+        <p>some messages...some messages...</p>
+      </div>
+    ),
+    onOk() {},
+  })
+}
+
+function success() {
+  MLModal.success({
+    content: 'some messages...some messages...',
+  })
+}
+
+function error() {
+  MLModal.error({
+    title: 'This is an error message',
+    content: 'some messages...some messages...',
+  })
+}
+
+function warning() {
+  MLModal.warning({
+    title: 'This is a warning message',
+    content: 'some messages...some messages...',
+  })
+}
+
+export const AlertModals = () => {
+  return (
+    <div>
+      <MLButton onClick={info}>Info</MLButton>
+      <MLButton onClick={success}>Success</MLButton>
+      <MLButton onClick={error}>Error</MLButton>
+      <MLButton onClick={warning}>Warning</MLButton>
+    </div>
+  )
 }
