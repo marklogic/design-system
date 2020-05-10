d40f3a9456b290eba74c07af1077b3a202e3a88f
Phoenix Eliot <phoenixjeliot@gmail.com>
2020-05-10 02:27:18 -0600

    Update gulp uniformity script

--- scripts/fix-uniformity-gulp.js
+++ scripts/fix-uniformity-gulp.js
@@ -6,6 +6,21 @@ const path = require('path')
 const _ = require('lodash')
 const { kebabCase } = require('lodash/string')
 
+const checkMultipleComponentsOneFile = () => {
+  return through.obj((file, enc, cb) => {
+    const code = file.contents.toString()
+    const matches = [...code.matchAll(/^const ML\w+/g)]
+    if (matches.length > 1) {
+      console.warn(`${path.basename(file.path)} has multiple components:`)
+      for (const match of matches) {
+        console.warn(`  ${match[0]}`)
+      }
+    }
+
+    return cb(null, file)
+  })
+}
+
 const insertStyleImport = () => {
   return through.obj((file, enc, cb) => {
     if (!file.isBuffer()) {
@@ -56,11 +71,17 @@ const addClassNames = () => {
   return through.obj((file, enc, cb) => {
     let madeChanges = false
     let code = file.contents.toString()
-    const componentName = path.basename(file.path).replace('.js', '')
-    const kebabCaseName = kebabCase(componentName).replace(/^m-l-/, 'ml-')
+    const mainComponentName = path.basename(path.dirname(file.path))
+    const childComponentName = path.basename(file.path).replace('.js', '')
+    let kebabCaseName
+    if (mainComponentName === childComponentName) {
+      kebabCaseName = 'ml-' + kebabCase(childComponentName.replace(/^ML/,''))
+    } else {
+      kebabCaseName = 'ml-' + kebabCase(mainComponentName.replace(/^ML/,'') + childComponentName.replace(/^ML/,''))
+    }
 
     {
-      const pattern = new RegExp(`(?<indents> +)<(?<antComponentName>${componentName.replace('ML', '')}) +\{\.\.\.props\} *(?<tagEndChar>(>|/>))`, 'g')
+      const pattern = new RegExp(`(?<indents> +)<(?<antComponentName>${childComponentName.replace('ML', '')}) +\{\.\.\.props\} *(?<tagEndChar>(>|/>))`, 'g')
       const matches = [...code.matchAll(pattern)]
       if (matches.length !== 0) {
         madeChanges = true
@@ -75,7 +96,7 @@ $<indents>$<tagEndChar>`)
       file.contents = Buffer.from(code)
     }
     {
-      const pattern = new RegExp(`^(?<indents> +)<(?<antComponentName>${componentName.replace('ML', '')})(?<otherProps>.*) \{\.\.\.props\} *(?<tagEndChar>(>|/>))`, 'g')
+      const pattern = new RegExp(`^(?<indents> +)<(?<antComponentName>${childComponentName.replace('ML', '')})(?<otherProps>.*) \{\.\.\.props\} *(?<tagEndChar>(>|/>))`, 'g')
       const matches = [...code.matchAll(pattern)]
       if (matches.length !== 0) {
         madeChanges = true
@@ -105,8 +126,9 @@ $<indents>$<tagEndChar>`)
 gulp.task('fix-uniformity', function(resolve) {
   return gulp.src(path.resolve(__dirname, '../src/ML*/ML*.js'))
     // .pipe(insertStyleImport())
+    .pipe(checkMultipleComponentsOneFile())
     .pipe(ensureImport("import './style'"))
     .pipe(ensureImport("import classNames from 'classnames'"))
     .pipe(addClassNames())
-    .pipe(gulp.dest(path.resolve(__dirname, '../tmp')))
+    .pipe(gulp.dest(path.resolve(__dirname, '../src')))
 })
