9fe453626cae913ef9e08a570012d395b56de51f
Dan Lipsitt <danlipsitt@gmail.com>
2020-05-13 16:07:43 -0700

    Convenience function to wrap icons in a hyperlink.

--- stories/13-Header.stories.js
+++ stories/13-Header.stories.js
@@ -6,19 +6,22 @@ export default {
   component: MLHeader,
 }
 
-export const Basic = () => (
-  <MLHeader
-    title='Data Hub Central' subtitle='Insurance Hub' backIcon={false}
-    avatar={{ src: 'https://www.marklogic.com/wp-content/themes/marklogic-bs4/resources/favicons/favicon-32x32.png' }}
-    extra={[
-      <MLIcon.Route key='route' />,
-      <span key='divider' style={{ 'border-left': '1px dashed' }} />,
-      // <span key='group' style={{ 'border-left': '1px dashed' }}>
-      <MLIcon.Search key='search' />,
-      <MLIcon.QuestionCircle key='help' />,
-      <MLIcon.SettingFilled key='settings' />,
-      <MLIcon.User key='user' />,
-      // </span>,
-    ]}
-  />
-)
+export const Basic = () => {
+  function makeIcon(icon, title) {
+    return <a href={`#${title}`} title={title} key={title}>{icon}</a>
+  }
+  return (
+    <MLHeader
+      title='Data Hub Central' subtitle='Insurance Hub' backIcon={false}
+      avatar={{ src: 'https://www.marklogic.com/wp-content/themes/marklogic-bs4/resources/favicons/favicon-32x32.png' }}
+      extra={[
+        makeIcon(<MLIcon.Route />, 'route'),
+        <span key='divider' style={{ 'border-left': '1px dashed' }} />,
+        makeIcon(<MLIcon.Search />, 'search'),
+        makeIcon(<MLIcon.QuestionCircle />, 'help'),
+        makeIcon(<MLIcon.SettingFilled />, 'settings'),
+        makeIcon(<MLIcon.User />, 'user'),
+      ]}
+    />
+  )
+}
