e61e9305908ec882892bf2eee9673d972ec95b74
Phoenix Eliot <phoenixjeliot@gmail.com>
2020-05-13 23:23:08 -0600

    Use classNames

--- src/MLHeader/MLHeader.js
+++ src/MLHeader/MLHeader.js
@@ -2,16 +2,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { PageHeader } from 'antd'
 import './style'
+import classNames from 'classnames'
 
 const MLHeader = (props) => {
-  const avatarProps = {size: 48, ...props.avatar}
+  const avatarProps = { size: 48, ...props.avatar }
   return (
     <PageHeader
-      className='ml-header'
       backIcon={false}
       ghost={false}
       {...props}
       avatar={avatarProps}
+      className={classNames('ml-header', props.className)}
     >
       {props.children}
     </PageHeader>
@@ -22,6 +23,6 @@ MLHeader.propTypes = {
   avatar: PropTypes.object,
   extra: PropTypes.arrayOf(PropTypes.node),
   title: PropTypes.node,
-};
+}
 
 export default MLHeader
