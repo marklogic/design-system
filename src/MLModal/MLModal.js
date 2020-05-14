6b82b2e364e8845f261731a190f580a1343521ac
Dan Lipsitt <danlipsitt@gmail.com>
2020-05-14 13:52:51 -0700

    Use full wrapper so we can set className on MLModal.

--- src/MLModal/MLModal.js
+++ src/MLModal/MLModal.js
@@ -1,5 +1,16 @@
+import React from 'react'
 import { DraggableModal } from 'ant-design-draggable-modal'
+import classNames from 'classnames'
 
-const MLModal = DraggableModal
+const MLModal = (props) => {
+  return (
+    <DraggableModal
+      {...props}
+      className={classNames('ml-modal', props.className)}
+    >
+      {props.children}
+    </DraggableModal>
+  )
+}
 
 export default MLModal
