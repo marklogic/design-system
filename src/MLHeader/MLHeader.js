b5b343a271d51c73ad308e8dd21b1ab15a400286
Phoenix Eliot <phoenixjeliot@gmail.com>
2020-05-15 12:05:28 -0600

    Clean up unused code

--- src/MLHeader/MLHeader.js
+++ src/MLHeader/MLHeader.js
@@ -1,12 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { MLPageHeader } from '@marklogic/design-system'
-import './style'
 import classNames from 'classnames'
 
-/**
- * Note that changes to `MLPageHeader` will affect this component.
- */
 const MLHeader = (props) => {
   return (
     <div
--- src/MLHeader/index.js
+++ src/MLHeader/index.js
@@ -1,1 +1,1 @@
-export {default} from './MLHeader'
+export { default } from './MLHeader'
