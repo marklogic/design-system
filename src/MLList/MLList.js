0ee1d753120e98e42834f60f087723e88337e3b2
Phoenix Eliot <phoenixjeliot@gmail.com>
2020-05-23 17:02:33 -0600

    Add List components and stories

---
+++ src/MLList/MLList.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { List } from 'antd'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+
+const MLList = (props) => (
+  <List
+    {...props}
+    className={classNames('ml-list', props.className)}
+  >
+    {props.children}
+  </List>
+)
+
+MLList.defaultProps = {
+  itemLayout: 'horizontal',
+}
+
+MLList.propTypes = {
+}
+
+export default MLList
---
+++ stories/53-List.stories.js
@@ -0,0 +1,126 @@
+import React from 'react'
+import { action } from '@storybook/addon-actions'
+import { withKnobs } from '@storybook/addon-knobs/react'
+import { MLList, MLDivider } from '@marklogic/design-system'
+
+// TODO: Replace with ML version when it's merged
+import {
+  Typography as MLTypography,
+  Avatar as MLAvatar,
+  Card as MLCard,
+} from 'antd'
+const MLText = MLTypography.Text
+
+export default {
+  title: 'Data Display/MLList',
+  decorators: [withKnobs],
+  parameters: {
+    info: {
+      text: 'Component description goes here',
+    },
+  },
+}
+
+export const basic = () => {
+  const data = [
+    'Racing car sprays burning fuel into crowd.',
+    'Japanese princess to wed commoner.',
+    'Australian walks 100km after outback crash.',
+    'Man charged over missing wedding girl.',
+    'Los Angeles battles huge wildfires.',
+  ]
+  return (
+    <div>
+      <MLDivider orientation='left'>Default Size</MLDivider>
+      <MLList
+        header={<div>Header</div>}
+        footer={<div>Footer</div>}
+        bordered
+        dataSource={data}
+        renderItem={item => (
+          <MLList.MLItem>
+            <MLText mark>[ITEM]</MLText> {item}
+          </MLList.MLItem>
+        )}
+      />
+      <MLDivider orientation='left'>Small Size</MLDivider>
+      <MLList
+        size='small'
+        header={<div>Header</div>}
+        footer={<div>Footer</div>}
+        bordered
+        dataSource={data}
+        renderItem={item => <MLList.MLItem>{item}</MLList.MLItem>}
+      />
+      <MLDivider orientation='left'>Large Size</MLDivider>
+      <MLList
+        size='large'
+        header={<div>Header</div>}
+        footer={<div>Footer</div>}
+        bordered
+        dataSource={data}
+        renderItem={item => <MLList.MLItem>{item}</MLList.MLItem>}
+      />
+    </div>
+  )
+}
+
+export const withMeta = () => {
+  const data = [
+    {
+      title: 'Ant Design Title 1',
+    },
+    {
+      title: 'Ant Design Title 2',
+    },
+    {
+      title: 'Ant Design Title 3',
+    },
+    {
+      title: 'Ant Design Title 4',
+    },
+  ]
+  return (
+    <MLList
+      itemLayout='horizontal'
+      dataSource={data}
+      renderItem={item => (
+        <MLList.MLItem>
+          <MLList.MLItem.MLMeta
+            avatar={<MLAvatar />}
+            title={<a href='https://ant.design'>{item.title}</a>}
+            description='Ant Design, a design language for background applications, is refined by Ant UED Team'
+          />
+        </MLList.MLItem>
+      )}
+    />
+  )
+}
+
+export const grid = () => {
+  const data = [
+    {
+      title: 'Title 1',
+    },
+    {
+      title: 'Title 2',
+    },
+    {
+      title: 'Title 3',
+    },
+    {
+      title: 'Title 4',
+    },
+  ]
+  return (
+    <MLList
+      grid={{ gutter: 16, column: 4 }}
+      dataSource={data}
+      renderItem={item => (
+        <MLList.MLItem>
+          <MLCard title={item.title}>Card content</MLCard>
+        </MLList.MLItem>
+      )}
+    />
+  )
+}
