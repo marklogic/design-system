c65d8e75006ddbd74b55528239835ca205b07271
Phoenix Eliot <phoenixjeliot@gmail.com>
2020-05-24 20:11:09 -0600

    Implement Pagination component and stories

---
+++ stories/6-Pagination.stories.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { action } from '@storybook/addon-actions'
+import { withKnobs, number, boolean, radios } from '@storybook/addon-knobs'
+import { MLPagination } from '@marklogic/design-system'
+
+export default {
+  title: 'Navigation/MLPagination',
+  decorators: [withKnobs],
+  parameters: {
+    info: {
+      text: 'Component description goes here',
+    },
+  },
+}
+
+export const basic = () => {
+  const showQuickJumper = radios('showQuickJumper', {
+    auto: 'auto',
+    true: 'true',
+    false: 'false',
+  }, 'auto')
+  const extras = {}
+  if (showQuickJumper === 'true') {
+    extras.showQuickJumper = true
+  } else if (showQuickJumper === 'false') {
+    extras.showQuickJumper = false
+  }
+  return (
+    <MLPagination
+      size={radios('size', ['default', 'small'], 'default')}
+      defaultCurrent={number('defaultCurrent', 1)}
+      total={number('total', 50)}
+      showSizeChanger={boolean('showSizeChanger', true)}
+      disabled={boolean('disabled', false)}
+      simple={boolean('simple', false)}
+      {...extras}
+      onChange={action('change')}
+      onShowSizeChange={action('onShowSizeChange')}
+    />
+  )
+}
