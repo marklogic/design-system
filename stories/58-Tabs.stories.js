ea38c2c79a16c2fb7ed9cf89e9d1110e2259e57b
Phoenix Eliot <phoenixjeliot@gmail.com>
2020-05-27 20:11:29 -0600

    Improve MLTabs story

--- stories/58-Tabs.stories.js
+++ stories/58-Tabs.stories.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { action } from '@storybook/addon-actions'
-import { withKnobs } from '@storybook/addon-knobs'
+import { radios, withKnobs } from '@storybook/addon-knobs'
 import { MLTabs } from '@marklogic/design-system'
 
 export default {
@@ -14,7 +14,21 @@ export default {
 }
 
 export const basic = () => {
+  const props = {
+    tabPosition: radios('tabPosition', ['left', 'top', 'right', 'bottom'], 'top'),
+    size: radios('size', ['small', 'default', 'large'], 'default'),
+  }
   return (
-    <MLTabs />
+    <MLTabs defaultActiveKey='1' onChange={action('onChange')} {...props}>
+      <MLTabs.MLTabPane tab='Tab 1' key='1'>
+        Content of Tab Pane 1
+      </MLTabs.MLTabPane>
+      <MLTabs.MLTabPane tab='Tab 2' key='2'>
+        Content of Tab Pane 2
+      </MLTabs.MLTabPane>
+      <MLTabs.MLTabPane tab='Tab 3' key='3'>
+        Content of Tab Pane 3
+      </MLTabs.MLTabPane>
+    </MLTabs>
   )
 }
