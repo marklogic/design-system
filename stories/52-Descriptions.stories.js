8f866c3e9cdfe2173d1cb553f77bf61e728de693
Phoenix Eliot <phoenixjeliot@gmail.com>
2020-05-27 19:55:13 -0600

    Update Descriptions component and story

--- stories/52-Descriptions.stories.js
+++ stories/52-Descriptions.stories.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { action } from '@storybook/addon-actions'
-import { withKnobs } from '@storybook/addon-knobs'
+import {boolean, number, radios, withKnobs} from "@storybook/addon-knobs";
 import { MLDescriptions } from '@marklogic/design-system'
 
 export default {
@@ -14,7 +14,28 @@ export default {
 }
 
 export const basic = () => {
+  const props = {
+    size: radios('size', ['default', 'middle', 'small'], 'default'),
+    bordered: boolean('bordered', false),
+    column: {
+      xxl: number('column.xxl', 4),
+      xl: number('column.xl', 3),
+      lg: number('column.lg', 3),
+      md: number('column.md', 3),
+      sm: number('column.sm', 2),
+      xs: number('column.xs', 1),
+    },
+
+  }
   return (
-    <MLDescriptions />
+    <MLDescriptions title='User Info' {...props}>
+      <MLDescriptions.MLItem label='UserName'>Zhou Maomao</MLDescriptions.MLItem>
+      <MLDescriptions.MLItem label='Telephone'>1810000000</MLDescriptions.MLItem>
+      <MLDescriptions.MLItem label='Live'>Hangzhou, Zhejiang</MLDescriptions.MLItem>
+      <MLDescriptions.MLItem label='Remark'>empty</MLDescriptions.MLItem>
+      <MLDescriptions.MLItem label='Address'>
+        No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China
+      </MLDescriptions.MLItem>
+    </MLDescriptions>
   )
 }
