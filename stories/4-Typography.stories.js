4b640ee4bbca4aee79e21aa89c4c62dcf2733dad
Phoenix Eliot <phoenixjeliot@gmail.com>
2020-06-05 14:13:03 -0600

    Improve MLTypography stories

--- stories/4-Typography.stories.js
+++ stories/4-Typography.stories.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { action } from '@storybook/addon-actions'
 import { withKnobs } from '@storybook/addon-knobs/react'
 import { MLTypography } from '@marklogic/design-system'
@@ -23,7 +23,7 @@ export const example = () => (
     </MLTypography.MLParagraph>
     <MLTypography.MLParagraph>
       After massive project practice and summaries, Ant Design, a design language for background
-      applications, is refined by Ant UED Team, which aims to
+      applications, is refined by Ant UED Team, which aims to&nbsp;
       <MLTypography.MLText strong>
         uniform the user interface specs for internal background projects, lower the unnecessary
         cost of design differences and implementation and liberate the resources of design and
@@ -54,7 +54,7 @@ export const example = () => (
   </MLTypography>
 )
 
-export const titles = () => (
+export const MLTitleComponent = () => (
   <div>
     <MLTypography.MLTitle>h1. Ant Design</MLTypography.MLTitle>
     <MLTypography.MLTitle level={2}>h2. Ant Design</MLTypography.MLTitle>
@@ -62,27 +62,74 @@ export const titles = () => (
     <MLTypography.MLTitle level={4}>h4. Ant Design</MLTypography.MLTitle>
   </div>
 )
+MLTitleComponent.story = {
+  name: 'MLTitle',
+}
 
-export const text = () => (
+export const MLTextComponent = () => (
   <div>
-    <MLTypography.MLText>Ant Design</MLTypography.MLText>
+    <MLTypography.MLText>Default</MLTypography.MLText>
     <br />
-    <MLTypography.MLText type="secondary">Ant Design</MLTypography.MLText>
+    <MLTypography.MLText type='secondary'>Secondary</MLTypography.MLText>
     <br />
-    <MLTypography.MLText type="warning">Ant Design</MLTypography.MLText>
+    <MLTypography.MLText type='warning'>Warning</MLTypography.MLText>
     <br />
-    <MLTypography.MLText type="danger">Ant Design</MLTypography.MLText>
+    <MLTypography.MLText type='danger'>Danger</MLTypography.MLText>
     <br />
-    <MLTypography.MLText disabled>Ant Design</MLTypography.MLText>
+    <MLTypography.MLText disabled>Disbaled</MLTypography.MLText>
     <br />
-    <MLTypography.MLText mark>Ant Design</MLTypography.MLText>
+    <MLTypography.MLText mark>Mark</MLTypography.MLText>
     <br />
-    <MLTypography.MLText code>Ant Design</MLTypography.MLText>
+    <MLTypography.MLText code>Code</MLTypography.MLText>
     <br />
-    <MLTypography.MLText underline>Ant Design</MLTypography.MLText>
+    <MLTypography.MLText underline>Underline</MLTypography.MLText>
     <br />
-    <MLTypography.MLText delete>Ant Design</MLTypography.MLText>
+    <MLTypography.MLText delete>Delete</MLTypography.MLText>
     <br />
-    <MLTypography.MLText strong>Ant Design</MLTypography.MLText>
+    <MLTypography.MLText strong>Strong</MLTypography.MLText>
+    <br />
+    <a>Link (anchor tag)</a>
+    <br />
+    <a className='link-color-hover-example'>(Hovered link)</a>
+  </div>
+)
+MLTextComponent.story = {
+  name: 'MLText',
+}
+
+export const editable = () => {
+  const [state, setState] = useState({ str: 'This is an editable text.' })
+  const onChange = str => {
+    console.log('Content change:', str)
+    setState({ str })
+  }
+  return (
+    <div>
+      <MLTypography.MLParagraph editable={{ onChange }}>{state.str}</MLTypography.MLParagraph>
+      <MLTypography.MLParagraph copyable>This is a copyable text.</MLTypography.MLParagraph>
+      <MLTypography.MLParagraph copyable={{ text: 'Hello, Ant Design!' }}>Replace copy text.</MLTypography.MLParagraph>
+    </div>
+  )
+}
+
+export const ellipsis = () => (
+  <div>
+    <MLTypography.MLParagraph ellipsis>
+      Ant Design, a design language for background applications, is refined by Ant UED Team. Ant
+      Design, a design language for background applications, is refined by Ant UED Team. Ant Design,
+      a design language for background applications, is refined by Ant UED Team. Ant Design, a
+      design language for background applications, is refined by Ant UED Team. Ant Design, a design
+      language for background applications, is refined by Ant UED Team. Ant Design, a design
+      language for background applications, is refined by Ant UED Team.
+    </MLTypography.MLParagraph>
+
+    <MLTypography.MLParagraph ellipsis={{ rows: 3, expandable: true }}>
+      Ant Design, a design language for background applications, is refined by Ant UED Team. Ant
+      Design, a design language for background applications, is refined by Ant UED Team. Ant Design,
+      a design language for background applications, is refined by Ant UED Team. Ant Design, a
+      design language for background applications, is refined by Ant UED Team. Ant Design, a design
+      language for background applications, is refined by Ant UED Team. Ant Design, a design
+      language for background applications, is refined by Ant UED Team.
+    </MLTypography.MLParagraph>
   </div>
 )
