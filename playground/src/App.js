14865d0b1e48adc09f360644f8f8032f997fe209
Phoenix Eliot <phoenixjeliot@gmail.com>
2020-06-10 22:53:21 -0600

    Update App.js

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 import {
+  MLAnchor,
   MLHeader,
   MLTooltip,
   MLButton,
@@ -53,7 +54,7 @@ export default class App extends Component {
       <div>
         <MLConfigProvider {...configValues}>
           <MLLayout>
-            <MLLayout.MLHeader style={{ padding: 0, position: 'fixed', zIndex: 1, width: '100%' }}>
+            <MLLayout.MLHeader style={{ padding: 0, position: 'fixed', zIndex: 2, width: '100%' }}>
               <MLHeader
                 title={text('title', 'Data Hub Central')}
                 avatar={
@@ -133,6 +134,29 @@ export default class App extends Component {
                   range
                   onChange={(v) => console.log(v)}
                 />
+              </div>
+              <div>
+                <div style={{ position: 'absolute', right: '50px' }}>
+                  <MLAnchor offsetTop={88}>
+                    <MLAnchor.MLLink href='#anchor-1' title='Link 1' />
+                    <MLAnchor.MLLink href='#anchor-2' title='Link 2' />
+                    <MLAnchor.MLLink href='#anchor-3' title='Link 3 with _target' target='_blank' />
+                    <MLAnchor.MLLink href='#anchor-4' title='Link 4'>
+                      <MLAnchor.MLLink href='#anchor-5' title='Link 5' />
+                      <MLAnchor.MLLink href='#anchor-6' title='Link 6' />
+                    </MLAnchor.MLLink>
+                    <MLAnchor.MLLink href='#anchor-7' title='Link 7' />
+                  </MLAnchor>
+                </div>
+                <div id='anchor-1'>anchor-1</div>
+                <div id='anchor-2' style={{ height: '500px' }}>anchor-2</div>
+                <div id='anchor-3' style={{ height: '500px' }}>anchor-3</div>
+                <div id='anchor-4' style={{ height: '500px' }}>
+                  anchor-4
+                  <div id='anchor-5' style={{ height: '200px' }}>anchor-5</div>
+                  <div id='anchor-6' style={{ height: '200px' }}>anchor-6</div>
+                </div>
+                <div id='anchor-7' style={{ height: '500px' }}>anchor-7</div>
               </div>
               <div style={{ height: 2000 }}>Some tall content</div>
             </MLLayout.MLContent>
