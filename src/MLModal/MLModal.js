61be4be29ec91856f0e484e63ce73740fc70a6cd
Phoenix Eliot <phoenixjeliot@gmail.com>
2020-07-29 12:26:14 -0600

    WIP Fix DraggableModal display issues

--- src/MLModal/MLModal.js
+++ src/MLModal/MLModal.js
@@ -2,18 +2,26 @@ import React from 'react'
 import { DraggableModal } from 'ant-design-draggable-modal'
 import classNames from 'classnames'
 
-const MLModal = React.forwardRef((props, ref) => {
+const MLModal = (props) => {
+  let className = ''
+  if (!props.title) {
+    className = 'ml-modal_no-title'
+  }
   return (
     <DraggableModal
-      ref={ref}
       initialHeight={0}
       {...props}
-      className={classNames('ml-modal', props.className)}
+      className={classNames('ml-modal', className, props.className)}
+      wrapClassName={classNames('ml-modal-wrap', props.wrapClassName)}
     >
       {props.children}
     </DraggableModal>
   )
-})
+}
+
+MLModal.defaultProps = {
+  mask: true,
+}
 
 MLModal.displayName = 'MLModal'
 
