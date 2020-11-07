0c84d4686bf8d596b35825da080ce8053cd6ac72
Jamie <56376825+jamie-fong@users.noreply.github.com>
2020-11-06 16:43:03 -0800

    Update 17-Select.stories.jsx

--- stories/17-Select.stories.jsx
+++ stories/17-Select.stories.jsx
@@ -7,7 +7,7 @@ import mdx from './17-Select.mdx'
 const { MLOption, MLOptGroup } = MLSelect
 
 export default {
-  title: 'Select',
+  title: 'Select (Dropdown)',
   decorators: [withKnobs],
   parameters: {
     fileName: '17-Select.stories.jsx',
